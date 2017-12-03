e73daa3c11a74a7576aa7e454d2154b3beff99aa
Jose Renteria <JoseRenteria.sf@outlook.com>
2017-12-03 10:08:22 -0800

    finished first web socket experiment

---
+++ server.js
@@ -0,0 +1,15 @@
+const io = require('socket.io')(),
+      port = 8000;
+
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+
+io.listen(port);
+console.log('listening on port ', port);
